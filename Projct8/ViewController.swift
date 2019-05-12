e468846f164e95f4a2a53f6f4f0cb55b9af9cb4d
@anninwonderland <anninwonderland@yandex.ru>
2019-05-12 22:07:03 +0300

    Initial Commit

---
+++ Projct8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Projct8
+//
+//  Created by Ann on 12/05/2019.
+//  Copyright © 2019 Ann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
